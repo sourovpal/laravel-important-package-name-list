28d03488e31d39f182d426cf8b0dfe6b8ab88864
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-02-13 13:43:51 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
 5. https://yajrabox.com/docs/laravel-datatables/10.0/installation
 6. https://github.com/rashidlaasri/LaravelInstaller
 7. https://spatie.be/docs/laravel-backup/v8/installation-and-setup
-8. 
+8. https://github.com/joedixon/laravel-translation
+9. 
