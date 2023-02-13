08ccf0332cb2eb6454589f7de80f28fc7d7f79c0
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-02-13 13:02:26 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+1. https://github.com/rap2hpoutre/laravel-log-viewer
+2. https://github.com/opcodesio/log-viewer
+3. https://github.com/barryvdh/laravel-debugbar
+4. https://spatie.be/docs/laravel-permission/v5/introduction
+5. https://yajrabox.com/docs/laravel-datatables/10.0/installation
+6. 
+7. 
